37987199323d7f6ca2820693ef75cfec1db3bbc3
KalebIsaias <kiscstudies@gmail.com>
2024-01-23 23:00:13 -0300

    feat: cadatrando user no banco pelo github

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,12 +1,16 @@
+import 'dotenv/config'
+
 import fastify from 'fastify'
 import cors from '@fastify/cors'
 import { memoriesRoutes } from './routes/memories'
+import { authRoutes } from './routes/auth'
 
 const server = fastify()
 
 server.register(cors, {
   origin: true,
 })
+server.register(authRoutes)
 server.register(memoriesRoutes)
 
 server.listen({
