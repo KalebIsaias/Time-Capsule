98f16d69db6d465e959ec3609c25094b1e077743
KalebIsaias <kiscstudies@gmail.com>
2024-01-25 20:37:17 -0300

    feat: adicionando rota para fazerupload

--- server/src/server.ts
+++ server/src/server.ts
@@ -3,11 +3,19 @@ import 'dotenv/config'
 import fastify from 'fastify'
 import cors from '@fastify/cors'
 import jwt from '@fastify/jwt'
+import multipart from '@fastify/multipart'
 import { memoriesRoutes } from './routes/memories'
 import { authRoutes } from './routes/auth'
+import { uploadRoutes } from './routes/upload'
+import { resolve } from 'node:path'
 
 const server = fastify()
 
+server.register(multipart)
+server.register(require('@fastify-static'), {
+  root: resolve(__dirname, '../uploads'),
+  prefix: '/uploads/',
+})
 server.register(cors, {
   origin: true,
 })
@@ -17,6 +25,7 @@ server.register(jwt, {
 
 server.register(authRoutes)
 server.register(memoriesRoutes)
+server.register(uploadRoutes)
 
 server.listen({
   port: 3333,
